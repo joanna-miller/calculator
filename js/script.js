825a6d3a1c20eecffbb5623d34527cfadd3cedb4
Jo <joannadawnmiller@gmail.com>
2021-01-11 17:32:39 -0500

    add subtract, add, multiply, and divide abilities in javascript
    Co-Authored By: Alan Call <alanmatthewcall@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
 function add(number1, number2) {
 	return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+alert((number1)+(" + ")+(number2)+(" = ")+(add(number1, number2)));
 
-alert(add(number1, number2));
+function multiply(number1, number2) {
+	return number1 * number2;
+}
+
+alert((number1)+(" x ")+(number2)+ (" = ")+(multiply(number1, number2)));
+
+function subtract(number1, number2) {
+	return number1 - number2;
+}
+
+alert((number1)+(" - ")+(number2)+(" = ")+(subtract(number1, number2)));
+
+function divide(number1, number2) {
+	return number1 / number2;
+}
+
+alert((number1)+(" / ")+(number2)+(" = ")+(divide(number1, number2)));
