7a58b1c2d847af85b98d63869d71383af0e86532
Jo <joannadawnmiller@gmail.com>
2021-01-13 18:28:51 -0500

    add user interface to calculator

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
 function add(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
 }
-
-alert((number1)+(" + ")+(number2)+(" = ")+(add(number1, number2)));
-
-function multiply(number1, number2) {
-	return number1 * number2;
-}
-
-alert((number1)+(" x ")+(number2)+ (" = ")+(multiply(number1, number2)));
 
 function subtract(number1, number2) {
-	return number1 - number2;
+  return number1 - number2;
 }
 
-alert((number1)+(" - ")+(number2)+(" = ")+(subtract(number1, number2)));
+function multiply(number1, number2) {
+  return number1 * number2;
+};
 
 function divide(number1, number2) {
-	return number1 / number2;
-}
+  return number1 / number2;
+};
 
-alert((number1)+(" / ")+(number2)+(" = ")+(divide(number1, number2)));
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+    event.preventDefault();
+		const number1 = parseInt($("#add1").val());
+		const number2 = parseInt($("#add2").val());
+		const result = add(number1, number2);
+    $("#output").text(result);
+	});
+});
